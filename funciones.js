8bbd624ec4e84ff7b7d30966403b783f360b729c
cristian <cristianbarrios1407@gmail.com>
2021-04-27 17:15:04 +0200

    Ejercicios 1, 2 y 4 perfectos

--- funciones.js
+++ funciones.js
@@ -2,6 +2,7 @@ function compruebaPar() {
 	
 	var numero = prompt("Introduce un número entero");
 	
+	//compruebo el modulo de 2 del numero
 	if(numero % 2 == 0) {
 		alert("El número "+numero+" es par");
 		return "par";
@@ -15,27 +16,33 @@ function compruebaPar() {
 function compruebaPalindromo(){
 	var palabra = prompt("Introduce una frase para comprobar si es palíndromo");
 
-      // Paso la fraseUser a minusculas y quito espacios
-      palabra = palabra.toLowerCase().replace(/\s/g,"");
+    // Paso la fraseUser a minusculas y quito espacios
+    palabra = palabra.toLowerCase().replace(/\s/g,"");
 	  
-      /* Creo otra frase partiendo de la de userFrase pero
-      la convierto en array, le doy la vuelta al array, lo paso a string*/
-      palabraVolteada = palabra.split("").reverse().toString();
+    /* Creo otra frase partiendo de la de userFrase pero
+    la convierto en array, le doy la vuelta al array, lo paso a string*/
+    palabraVolteada = palabra.split("").reverse().toString();
 	  
-      // Le quito las "," con un remplace dentro del for
-      // Lo igualo a -1 ya que tiene una coma menos que el número total de letras
-      for (var i = 0; i < ((palabraVolteada.length)-1); i++) {
-        palabraVolteada = palabraVolteada.replace(",","");
-      };
-      // Comparo las dos frases.
-      if(palabra == palabraVolteada){
-        // Si el resultado es positivo
-        resultado = "es un Palindromo";
-      }
-      else{
-        resultado = "no es un Palindromo";
-      }
-      // Muestro el el resultado
-      alert("Tu frase "+resultado);
+    // Le quito las "," con un remplace dentro del for
+    for (var i = 0; i < ((palabraVolteada.length)-1); i++) {
+      palabraVolteada = palabraVolteada.replace(",","");
+    };
+    // Comparo las dos frases.
+    if(palabra == palabraVolteada){
+      // Si el resultado es positivo
+      resultado = "es un Palindromo";
     }
+    else{
+      resultado = "no es un Palindromo";
+    }
+    // Muestro el el resultado
+    alert("Tu frase "+resultado);
+}
+
+function volteaPalabra(){
+	
+	var palabra = prompt("Introduce una palabra para voltearla");
+	var palabraVuelta = palabra.split("").reverse().toString();
+  
+	alert("Palabra volteada: " + palabraVuelta);
 }
